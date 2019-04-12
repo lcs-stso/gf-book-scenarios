ca30894b5c81bc0594f24d5c48805a0949cdcee7
Sarah Tso <stso.lcs.on.ca>
2019-04-12 15:22:59 -0400

    Used parameters in the constructor...using abstraction to make the Key class work for many different notes

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -6,15 +6,27 @@ public class Key extends Actor
      * Instance variables
      */
     //Track whether is "piano key down" image has already been set or not
+    //(isDown is a primitive data type - shows in red,type is lowercase)
     private boolean isDown; 
+    
+    //Track what key is being played AND what sound file to play 
+    //(key and sound are object data types - shoes in black, type is Capitalized)
+    private String key;                //**
+    private String sound;              //**
+    
     /**
      * Create a new key.
      * Constructor - it runs once when a Key object is created 
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
         //Key begins in the "up"position
         isDown = false; 
+        
+        //Initialize the key and sound instance variables //**
+        key = keyName;                                    //**
+        sound = soundFile;                                //**
+        
     }
 
     /**
@@ -25,13 +37,13 @@ public class Key extends Actor
         //When the key image is NOT already showing"down"
         //AND
         //the "g" key on the keyboard has been pressed
-        if( !isDown && Greenfoot.isKeyDown("g"))
+        if( !isDown && Greenfoot.isKeyDown(key))
         {
             setImage("white-key-down.png"); 
             isDown = true;
             play();
         }
-        if ( isDown == true && Greenfoot.isKeyDown ("g") == false ) 
+        if ( isDown == true && Greenfoot.isKeyDown (key) == false ) 
         {
             setImage("white-key.png");
             isDown = false; 
@@ -43,7 +55,7 @@ public class Key extends Actor
      */
     public void play()
     {
-        Greenfoot.playSound("3a.wav"); 
+        Greenfoot.playSound(sound +".wav"); 
     }
 }
 
