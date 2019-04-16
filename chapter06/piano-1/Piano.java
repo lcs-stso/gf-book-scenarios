f1e5b5053343f1752e203a806207f78aed9dc93f
Sarah Tso <stso.lcs.on.ca>
2019-04-16 11:37:31 -0400

    Got the piano working

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -16,24 +16,25 @@ public class Piano extends World
     //Object data type..square brackets[]indicate an array(a list) 
     //Data type: String,we want an array,and the name is"student name"
     String[] studentNames = {"Sarah", "Chelsea","Jack","Maxwell","Gavin","Peter",
-           "Minah","Brandon","Jeowoo","Peter","Justy"};
+            "Minah","Brandon","Jeowoo","Peter","Justy"};
     //Track the white keys and notes
     String[] whiteKeys = {"a","s","d","f","g","h","j","k","l",";","'","\\"};
     String[] whiteNotes = {"3c","3d","3e","3f","3g","3a","3b","4c","4d","4e",
-    "4f","4g"}; 
+            "4f","4g"};
     /**
      * Make the piano.
      */
     public Piano() 
     {
         super(800, 340, 1);
-        
+
         //Start tracking frames 
         frames = 0;
-        
+
         //Initialize the array of student names 
-        
+
     }
+
     /**
      * Act- called repeatedly to create animation 
      */
@@ -44,11 +45,28 @@ public class Piano extends World
         //The showText statement only runs when BOTH conditions are true 
         //This block of code runs once per second until the end of the 
         //whiteKeys array
-        if((frames%60 == 0) && (frames/60 < 10))
+        //NOTE: whiteKeys.length automatically returns the correct number 
+        //      of values in the array(so that we wont run pass the end) 
+        int position = frames/60;
+        if((frames%60 == 0) && (position < whiteKeys.length))
         {
-            
+            //Assemble the piano by creating each key one by one 
+            Key anotherKey = new Key(whiteKeys[position],whiteNotes[position]);
+            //This line actually adds the object to the Piano world
+            //     OBJECT    HORIZONTAL POSITION   VERTICAL POSITION  
+            //NOTE: Check phone to see sketch of logical explaining the 
+            //expression
+            //      for the x position(horizontal position) of each key 
+
+            addObject(anotherKey,54 + position * 63,140);
+
+            //Only show a message when we are in the bounds of the array
+            showText("Array index is:" + frames / 60,400,250);
+
+            //Only say hello when we are in the bounds of the array
+            //Say hello to everyone in the class!
             //showText("Hello" + studentNames[ frames/60 ],400,170);
-            
+
         }
         //Keep track of scores 
         frames+=1; 
